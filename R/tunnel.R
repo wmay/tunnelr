70873c79ce40168545284737d57c987aa1aa8c25
William May <williamcmay@live.com>
2016-05-24 15:01:43 -0400

    Added portOpen command and started windowsTunnel function

--- R/tunnel.R
+++ R/tunnel.R
@@ -1,5 +1,27 @@
 # ssh tunneling from R
 
+portOpen = function(n) {
+  flags = paste0("-tnl | grep :", n, " | wc -l")
+  x = system2("netstat", flags, stdout = T)
+  as.numeric(x) == 0
+}
+
+windowsTunnel = function(remote_server, server_username,
+                         remote_port = 3306, local_port = 9000) {
+  "start plink -N -ssh wm177874@projpet.rit.albany.edu -L 9000:projpet.rit.albany.edu:3306"
+  flags = paste0("start plink -N -ssh ", server_username, "@",
+                 remote_server, " -L ", local_port, ":",
+                 remote_server, ":", remote_port)
+  x = system2("ssh", flags)
+  if (x != 0) stop("SSH failed.")
+  con = list(remote_server = remote_server,
+             server_username = server_username,
+             remote_port = remote_port,
+             local_port = local_port)
+  class(con) = "tunnel"
+  con
+}
+
 #' Set up SSH tunneling
 #' 
 #' @param remote_server The URL or IP address of the remote server.
@@ -17,18 +39,24 @@
 #' @export
 tunnel = function(remote_server, server_username,
                   remote_port = 3306, local_port = 9000) {
-  flags = paste0("-fN -L ", local_port, ":localhost:",
-                 remote_port, " ", server_username,
-                 "@", remote_server,
-                 ' -o "ExitOnForwardFailure yes"')
-  x = system2("ssh", flags)
-  if (x != 0) stop("SSH failed.")
-  con = list(remote_server = remote_server,
-             server_username = server_username,
-             remote_port = remote_port,
-             local_port = local_port)
-  class(con) = "tunnel"
-  con
+  if (.Platform$OS.type == "unix") {
+    if (!portOpen(local_port)) stop("Port is already in use.")
+    flags = paste0("-fN -L ", local_port, ":localhost:",
+                   remote_port, " ", server_username,
+                   "@", remote_server,
+                   ' -o "ExitOnForwardFailure yes"')
+    x = system2("ssh", flags)
+    if (x != 0) stop("SSH failed.")
+    con = list(remote_server = remote_server,
+               server_username = server_username,
+               remote_port = remote_port,
+               local_port = local_port)
+    class(con) = "tunnel"
+    con
+  } else {
+    windowsTunnel(remote_server, server_username, remote_port,
+                  local_port)
+  }
 }
 
 #' Close an SSH tunnel
